e8614bae74dc09027891aacbe250b5d966a6efb7
Meganathan <meganathan212003@gmail.com>
2025-02-01 17:37:26 +0530

    change the code

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.use(
 );
 
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+ }); 
 
 const storage = multer.diskStorage({
    destination: (req, file, cb) => {
